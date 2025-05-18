397d4ea196d4bf4e2e8296503cf9e9b6fbf117d4
Raphael Padua <raph.escpad@gmail.com>
2025-05-18 12:06:33 -0700

    feat: add parallax effect with scrolling layers with 2D camera

--- mechanics/01_parallax/scenes/player.gd
+++ mechanics/01_parallax/scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
-	var dir := Input.get_axis("ui_left", "ui_right")   # -1, 0, or +1
+	var dir := Input.get_axis("ui_left", "ui_right")
 	velocity.x = dir * 150      
 	
-	move_and_slide()                # e.g. 150
+	move_and_slide()
